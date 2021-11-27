1c782bfe95c30396d5e766ed77fe60e3efe8742d
Alfyu07 <w.alfa433@gmail.com>
2021-11-28 07:15:48 +0800

    fix ui tutorial

--- lib/presentation/ui/pages/tutorial_page.dart
+++ lib/presentation/ui/pages/tutorial_page.dart
@@ -41,67 +41,66 @@ class TutorialPageState extends State<TutorialPage> {
         toolbarHeight: 80,
         backgroundColor: mainColor,
       ),
-      body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            carouselSlider = CarouselSlider(
-              options: CarouselOptions(
-                  height: 400.0,
-                  initialPage: 0,
-                  enlargeCenterPage: true,
-                  autoPlay: false,
-                  reverse: false,
-                  enableInfiniteScroll: true,
-                  autoPlayInterval: Duration(seconds: 2),
-                  autoPlayAnimationDuration: Duration(milliseconds: 2000),
-                  scrollDirection: Axis.horizontal,
-                  onPageChanged: (index, reason) {
-                    setState(() {
-                      _current = index;
-                    });
-                  }),
-              items: imgList.map((imgUrl) {
-                return Builder(
-                  builder: (BuildContext context) {
-                    return Container(
-                      width: MediaQuery.of(context).size.width,
-                      margin: EdgeInsets.symmetric(horizontal: 10.0),
-                      decoration: BoxDecoration(
-                        color: Colors.green,
-                      ),
-                      child: Image.network(
-                        imgUrl,
-                        fit: BoxFit.fill,
-                      ),
-                    );
-                  },
-                );
-              }).toList(),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: map<Widget>(imgList, (index, url) {
-                return Container(
-                  width: 10.0,
-                  height: 10.0,
-                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: _current == index ? Colors.redAccent : Colors.green,
-                  ),
-                );
-              }),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-          ],
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          carouselSlider = CarouselSlider(
+            options: CarouselOptions(
+                height: 400.0,
+                initialPage: 0,
+                enlargeCenterPage: true,
+                autoPlay: false,
+                reverse: false,
+                enableInfiniteScroll: true,
+                autoPlayInterval: const Duration(seconds: 2),
+                autoPlayAnimationDuration: const Duration(milliseconds: 2000),
+                scrollDirection: Axis.horizontal,
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    _current = index;
+                  });
+                }),
+            items: imgList.map((imgUrl) {
+              return Builder(
+                builder: (BuildContext context) {
+                  return Container(
+                    width: MediaQuery.of(context).size.width,
+                    margin: const EdgeInsets.symmetric(horizontal: 10.0),
+                    decoration: const BoxDecoration(
+                      color: ghostWhiteColor,
+                    ),
+                    child: Image.network(
+                      imgUrl,
+                      fit: BoxFit.fill,
+                    ),
+                  );
+                },
+              );
+            }).toList(),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: map<Widget>(imgList, (index, url) {
+              return Container(
+                width: 10.0,
+                height: 10.0,
+                margin:
+                    const EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: _current == index ? mainColor : ghostWhiteColor,
+                ),
+              );
+            }),
+          ),
+          const SizedBox(
+            height: 20.0,
+          ),
+        ],
       ),
     );
   }
